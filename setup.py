16ddfc337ebbae682b38844ad9ab9d867a00ee1b
Jonathan Yu <jonathan.yu@csiro.au>
2017-08-22 14:49:43 +1000

    updating manifest and setup to include data files in package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ldrpyutils',
-    version='0.2',
+    version='0.2.1',
     packages=['ldrpyutils'],
     classifiers=[
         'Development Status :: 3 - Alpha',
